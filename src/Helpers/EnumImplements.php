cac3dcf6d64dc0aff2b540b5bd1d63fb354abba8
henzeb <henzeberkheij@gmail.com>
2022-06-17 10:50:05 +0200

    taking out laravel function

--- src/Helpers/EnumImplements.php
+++ src/Helpers/EnumImplements.php
@@ -12,7 +12,7 @@ abstract class EnumImplements
     {
         EnumCheck::check($class);
 
-        return in_array($implements, class_uses_recursive($class));
+        return in_array($implements, self::classUsesTrait($class));
     }
 
     public static function mappers(string $class): bool
@@ -28,5 +28,27 @@ abstract class EnumImplements
     public static function state(string $class): bool
     {
         return self::traitOn($class, State::class);
+    }
+
+    private static function classUsesTrait(string $class)
+    {
+        $results = [];
+
+        foreach (array_reverse(class_parents($class)) + [$class => $class] as $class) {
+            $results += self::traitUsesTrait($class);
+        }
+
+        return array_unique($results);
+    }
+
+    private static function traitUsesTrait(string $trait): array
+    {
+        $traits = class_uses($trait) ?: [];
+
+        foreach ($traits as $trait) {
+            $traits += self::traitUsesTrait($trait);
+        }
+
+        return $traits;
     }
 }
